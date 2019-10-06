d3d588f8e9647fce1d44f38153a103e9c9ebbf1d
KirillFilipp <kirill_filipp@mail.ru>
2019-10-07 00:09:04 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+  let str1 = str.split('');
+  let f, f1;
+  let result = true;
+  function findConfig(ch, bracketsConfig) {
+    return bracketsConfig.find((elem) => {
+      return elem[0] === ch || elem[1] === ch;
+    });
+  }
+  if (str1.length % 2 === 1) {
+    return false;
+  }
+  for (f = 0; f < str1.length; f++) {
+    let config = findConfig(str[f], bracketsConfig);
+    if (config === undefined) return false;
+    if (str1[f] === config[0] && stack.indexOf(config[1]) === -1) {
+      stack.push(str1[f]);
+    } else if (str1[f] === config[1]) {
+      if (stack[stack.length - 1] === config[0]) {
+        stack.pop();
+      }
+    }
+  }
+  if (stack.length !== 0) {
+    return false;
+  } else {
+    return true;
+  }
 }
